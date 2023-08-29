19d2f0c140d185e3a8f52f9f0a61cb3e0432719a
Saadat Rafid Ahmed <63824872+saadat-r-ahmed@users.noreply.github.com>
2023-08-29 23:05:58 +0600

    updated dependencies

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['opencv-python', 'pyautogui', 'pyaudio', 'pandas'],
-    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    install_requires=['pandas', 'joblib'],
+    keywords=['python', 'sentiment classification', 'nlp'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
